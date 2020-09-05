9c5cd4670bd36d202ad4652919b50509a8018773
zderApfel <scoutt172@yahoo.com>
2020-09-05 11:52:47 -0400

    Password and confirm password fields done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const email = document.getElementById("email");
 const zipCode = document.getElementById("zip");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-pass");
 
 email.addEventListener("input", function(event){
     if(email.validity.typeMismatch){
@@ -19,3 +21,12 @@ zipCode.addEventListener("input", function(event){
         zipCode.setCustomValidity("");
     }
 });
+
+confirmPassword.addEventListener("input", function(event){
+    if(confirmPassword.value != password.value){
+        confirmPassword.setCustomValidity("Passwords don't match!");
+    }
+    else{
+        confirmPassword.setCustomValidity("");
+    }
+})
